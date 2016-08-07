73e2f3385fbf330ebd6b79df5234e045e0cb7acc
akwido7 <riektg@gmail.com>
2016-08-07 23:48:52 +0300

    Create base project structure create MainActivity & layout

--- RemindMe/app/src/main/java/w72016/com/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/w72016/com/remindme/MainActivity.java
@@ -1,19 +1,38 @@
 package w72016.com.remindme;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.AppDefault); //примениенеие стилей
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
+
+        initToolbar();
+        initNavigationView();
+    }
+
+    private void initToolbar() {
+
+    }
+
+    private void initNavigationView(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 
     }
